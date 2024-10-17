37aea8db7cd4b4c4d35ee6a2b9f793ebffa57395
HIG <hingjy1997@gmail.com>
2024-10-17 17:42:35 +0900

    최초 커밋

---
+++ src/main/java/com/example/basicsamplesite/BasicSampleSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.basicsamplesite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSampleSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicSampleSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/basicsamplesite/BasicSampleSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basicsamplesite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSampleSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
